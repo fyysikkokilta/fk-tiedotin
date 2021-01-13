1185e41160c9019bda678f44dae449d1983ffe01
Niko Savola <niko.savola@aalto.fi>
2021-01-13 12:18:27 +0200

    Added script for uploading to Fyysikkokilta website

---
+++ upload-to-otax.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+week=$(date +%U)
+((week++))
+week=$(LC_NUMERIC="en_US.UTF-8" printf "%.2d" $((week)))
+echo "The week number is $week."
+
+scp mails/kilta-tiedottaa-viikko-${week}.html fk@otax.fi:~/www/wp-content/uploads/viikkotiedote &&
+scp data/week${week}.jpg fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data &&
+scp data/week${week}-en.jpg fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data
+
+echo "Success!"
