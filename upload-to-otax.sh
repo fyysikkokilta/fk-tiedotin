3ae1759dd32b77335175899b4c411743622880e1
Niko Savola <niko.savola@aalto.fi>
2021-01-13 12:25:44 +0200

    .jpg -> .json fix

--- upload-to-otax.sh
+++ upload-to-otax.sh
@@ -6,7 +6,7 @@ week=$(LC_NUMERIC="en_US.UTF-8" printf "%.2d" $((week)))
 echo "The week number is $week."
 
 scp mails/kilta-tiedottaa-viikko-${week}.html fk@otax.fi:~/www/wp-content/uploads/viikkotiedote &&
-scp data/week${week}.jpg fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data &&
-scp data/week${week}-en.jpg fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data
+scp data/week${week}.json fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data &&
+scp data/week${week}-en.json fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data
 
 echo "Success!"
