3e5caf7163d922ef384b3bdea84191f5bb8a46e0
Niko Savola <niko.savola@aalto.fi>
2021-01-20 17:47:53 +0200

    Added year to upload script

--- upload-to-otax.sh
+++ upload-to-otax.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 week=$(date +%U)
+year=$(date +%Y)
 ((week++))
 week=$(LC_NUMERIC="en_US.UTF-8" printf "%.2d" $((week)))
 echo "The week number is $week."
 
-scp mails/kilta-tiedottaa-viikko-${week}.html fk@otax.fi:~/www/wp-content/uploads/viikkotiedote &&
-scp data/week${week}.json fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data &&
-scp data/week${week}-en.json fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data
+scp mails/kilta-tiedottaa-viikko-${week}.html fk@otax.fi:~/www/wp-content/uploads/viikkotiedote/${year} &&
+scp data/week${week}.json fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data/${year} &&
+scp data/week${week}-en.json fk@otax.fi:~/www/wp-content/uploads/viikkotiedote-data/${year}
 
 echo "Success!"
